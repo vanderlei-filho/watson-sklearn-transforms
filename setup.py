8c46dcca21d6dd2c23afb4a858d2b77d56bbf050
Vanderlei Pereira <33144973+vnderlev@users.noreply.github.com>
2020-08-06 17:14:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_tansforms/',
+      url='https://github.com/vnderlev/sklearn_transforms',
       author='Vanderlei Munhoz',
       author_email='vnderlev@protonmail.ch',
       license='BSD',
