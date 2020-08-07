5ab47e5cada9bcefbd66782d50edf2a86005dcb5
Vanderlei Munhoz Pereira Filho <vnderlev@protonmail.ch>
2020-08-07 05:42:54 -0300

    added comments in PT-BR

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,18 +1,16 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-# import numpy.lib.recfunctions as rf
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-# You can declare several custom transforms (as different classes) in
-# this file
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
 
-    def transform(self, X):
-        return X.copy().drop(labels=self.columns, axis='columns')
-        # data = X.copy()
-        # return rf.drop_fields(data, self.columns)
-
     def fit(self, X, y=None):
         return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.drop(labels=self.columns, axis='columns')
