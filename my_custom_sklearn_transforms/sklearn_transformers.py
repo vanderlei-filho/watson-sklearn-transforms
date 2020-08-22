7063b4e0d095478dd6837b8dd6c58ebefbb279ce
Juan Martin Leroux <juan_martin_213@hotmail.com>
2020-08-21 23:14:23 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,3 +1,4 @@
+# All sklearn Transforms must have the `transform` and `fit` methods
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -8,9 +9,16 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        data = data.drop(labels=self.columns, axis='columns')
+        data = data.dropna(axis=0, subset=["NOTA_GO"])
+        df = df_data_1[['NOTA_DE' , 'NOTA_EM' , 'NOTA_MF' , 'NOTA_GO']]
+        data['MEDIA'] = df.mean(axis=1)
+        data['SOMA'] = df.sum(axis=1)
+        df = df_data_1[['NOTA_DE' , 'NOTA_EM', 'NOTA_GO']]
+        data['MEDIA_HUMANAS'] = df.mean(axis=1)
+        return data
