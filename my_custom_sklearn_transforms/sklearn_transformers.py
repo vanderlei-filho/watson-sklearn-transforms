387e59982b9088217866b93603e367645b3a6740
Juan Martin Leroux <juan_martin_213@hotmail.com>
2020-08-21 23:54:45 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -22,3 +22,37 @@ class DropColumns(BaseEstimator, TransformerMixin):
         df = data[['NOTA_DE' , 'NOTA_EM', 'NOTA_GO']]
         data['MEDIA_HUMANAS'] = df.mean(axis=1)
         return data
+
+class Coding():
+    def __init__(self, p):
+        self.p = p
+
+    def decode(self):
+        self.p1 = np.array([])
+        for i in range(0,len(self.p)):
+            if self.p[i] <= 1.0:
+                self.p1 = np.append(self.p1, "EXCELENTE")
+            if self.p[i] == 2.0:
+                self.p1 = np.append(self.p1, "MUITO_BOM")
+            if self.p[i] == 3.0:
+                self.p1 = np.append(self.p1, "HUMANAS")
+            if self.p[i] == 4.0:
+                self.p1 = np.append(self.p1, "EXATAS")
+            if self.p[i] >= 5.0:
+                self.p1 = np.append(self.p1, "DIFICULDADE")
+        return self.p1
+
+    def code(self):
+        self.p1 = np.array([])
+        for i in range(0,len(self.p)):
+            if self.p[i] == "EXCELENTE":
+                self.p1 = np.append(self.p1, 1)
+            if self.p[i] == "MUITO_BOM":
+                self.p1 = np.append(self.p1, 2)
+            if self.p[i] == "HUMANAS":
+                self.p1 = np.append(self.p1, 3)
+            if self.p[i] == "EXATAS":
+                self.p1 = np.append(self.p1, 4)
+            if self.p[i] == "DIFICULDADE":
+                self.p1 = np.append(self.p1, 5)
+        return self.p1
