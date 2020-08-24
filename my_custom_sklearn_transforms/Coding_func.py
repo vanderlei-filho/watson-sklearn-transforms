8b8eba2b6d1f7b9bfe4d8a2461fc47f3e4865b87
Juan Martin Leroux <juan_martin_213@hotmail.com>
2020-08-24 15:57:58 -0300

    Update Coding_func.py

--- my_custom_sklearn_transforms/Coding_func.py
+++ my_custom_sklearn_transforms/Coding_func.py
@@ -1,24 +1,16 @@
 import numpy as np
-class Coding():
-    def __init__(self, p):
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class Coding(BaseEstimator, TransformerMixin):
+     def __init__(self, p):
         self.p = p
 
-    def decode(self):
-        self.p1 = np.array([])
-        for i in range(0,len(self.p)):
-            if self.p[i] <= 1.0:
-                self.p1 = np.append(self.p1, "EXCELENTE")
-            if self.p[i] == 2.0:
-                self.p1 = np.append(self.p1, "MUITO_BOM")
-            if self.p[i] == 3.0:
-                self.p1 = np.append(self.p1, "HUMANAS")
-            if self.p[i] == 4.0:
-                self.p1 = np.append(self.p1, "EXATAS")
-            if self.p[i] >= 5.0:
-                self.p1 = np.append(self.p1, "DIFICULDADE")
-        return self.p1
-
-    def code(self):
+    def fit(self):
+        return self.p
+    
+    def transform(self):
         self.p1 = np.array([])
         for i in range(0,len(self.p)):
             if self.p[i] == "EXCELENTE":
