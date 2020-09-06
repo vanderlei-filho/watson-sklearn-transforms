a3dea04f26233b221e1344a00958424b572c150b
reynagasa <69727218+reynagasa@users.noreply.github.com>
2020-09-06 12:29:07 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -80,3 +80,19 @@ class MinimColumns(BaseEstimator, TransformerMixin):
         
         # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return data.drop(labels=self.columns, axis='columns')
+
+    
+    
+# RR Desafio2    
+class DropColumnsDesaf2(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
+        return data.drop(labels=self.columns, axis='columns')
